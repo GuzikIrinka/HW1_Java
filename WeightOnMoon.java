c5441f87baf2c7bd0145b45dce4c26c1795edd2b
Irina <andrey_k.90@mail.ru>
2022-11-13 23:21:34 +0300

    Вес на Луне вычислен, корректировка

--- WeightOnMoon.java
+++ WeightOnMoon.java
@@ -7,6 +7,7 @@ public class WeightOnMoon {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Input your weight:");
-        System.out.printf("Your weight on the moon is: " + "%.2f", scanner.nextDouble() * 0.17);
+        System.out.printf("Your weight on the Moon is: " + "%.2f", scanner.nextDouble() * 0.17);
+        scanner.close();
     }
 }
